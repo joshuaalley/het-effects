d3c6e565f1b5bbe941859c91845962f56d1cee96
Joshua Alley <joshua.alley@ucd.ie>
2023-09-05 14:17:12 +0100

    tomz and weeks estimates

--- data/tomz-weeks-rep/tomz-weeks-reanalysis.R
+++ data/tomz-weeks-rep/tomz-weeks-reanalysis.R
@@ -56,11 +56,22 @@ tw.het.treat <- brm(bf(force ~ 1 + white + male + hawk + intl + natl.sup +
 summary(tw.het.treat)
 
 # Using marginaleffects out of the box 
+
+grid.het.treat <- tw.rep %>%
+  select(alliance, regime, stakes,
+         costs, region, treat.group) %>%
+  distinct() %>%
+  mutate(
+    white = median(tw.rep$white),
+    male = median(tw.rep$male),
+    hawk = median(tw.rep$hawk),
+    intl =  median(tw.rep$intl),
+    natl.sup = median(tw.rep$natl.sup)
+  )
+
 # predictions 
 pred.het.treat <- predictions(tw.het.treat,
-            newdata = datagrid(model = tw.het.treat,
-                       alliance = c(0, 1),
-                       treat.group = unique(tw.rep$treat.group))) %>%
+            newdata = grid.het.treat) %>%
                        posterior_draws()
 
 ggplot(pred.het.treat, aes(x = draw, y = treat.group, 
@@ -73,11 +84,30 @@ ggplot(pred.het.treat, aes(x = draw, y = treat.group,
 # slopes- create groups
 slopes.het.treat <- slopes(model = tw.het.treat,
                            variables = "alliance",
-                           newdata = datagrid(
-                             treat.group = unique(tw.rep$treat.group))
-)
+                           newdata = grid.het.treat)
 slopes.het.treat
 
+ggplot(slopes.het.treat, aes(y = estimate, x = region,
+                             shape = factor(costs),
+                             color = factor(stakes))) +
+  facet_wrap(~ regime, ncol = 5,
+             labeller = labeller(regime = c(`0` = "Autocracy",
+                                            `1` = "Democracy"))) +
+  geom_hline(yintercept = 0) +
+  geom_pointrange(aes(ymin = conf.low, ymax = conf.high),
+                  position = position_dodge(width = 1)) +
+  scale_color_grey(name = "Stakes",
+                   labels = c(`0` = "Low",
+                              `1` = "High")) +
+  scale_shape_discrete(name = "Costs",
+                       labels = c(`0` = "High",
+                                  `1` = "Low")) +
+  labs(title = "Heterogeneous Treatments",
+       subtitle = c("Region, Regime, Stakes, Cost"),
+       x = "Region", 
+       y = "Marginal effect of Alliance")
+
+# give posterior mass
 het.treat.all <- posterior_draws(slopes.het.treat)
 
 ggplot(het.treat.all, aes(x = draw, y = treat.group)) +
@@ -87,7 +117,8 @@ ggplot(het.treat.all, aes(x = draw, y = treat.group)) +
 
 
 ### model with treatment heterogeneity
-tw.rep$het.group <- paste(tw.rep$white, tw.rep$male, tw.rep$hawk,
+tw.rep$het.group <- paste(tw.rep$white, tw.rep$male,
+                          tw.rep$intl, tw.rep$hawk,
                             sep = "_")
 treat.het.prior <- c(
   prior(normal(0, .5), class = "b"),
@@ -95,7 +126,7 @@ treat.het.prior <- c(
 )
 tw.treat.het <- brm(bf(force ~ 1 +
                          regime + stakes + costs + region +
-                         alliance*(white + male + hawk) +
+                         alliance*(white + male + intl + hawk) +
                          (1 + alliance | het.group) ),
                     data = tw.rep,
                     prior = treat.het.prior,
@@ -108,30 +139,61 @@ tw.treat.het <- brm(bf(force ~ 1 +
 summary(tw.treat.het)
 
 
-# predictions 
+# predictions
+# get data 
+grid.treat.het <- tw.rep %>%
+                      select(alliance, white, male,
+                           intl, hawk, het.group) %>%
+                      distinct() %>%
+                      mutate(
+                        regime = median(tw.rep$regime),
+                        stakes = median(tw.rep$stakes),
+                        region = median(tw.rep$region),
+                        costs =  median(tw.rep$costs)
+                      )
+
 pred.treat.het <- predictions(tw.treat.het,
-                              newdata = datagrid(model = tw.treat.het,
-                                                 alliance = c(0, 1),
-                                                 het.group = unique(tw.rep$het.group))) %>%
+                              newdata = grid.treat.het) %>%
   posterior_draws()
 
 ggplot(pred.treat.het, aes(x = draw, y = het.group, 
-                           fill = factor(alliance))) +
-  stat_halfeye(slab_alpha = .5) +
+                          color = factor(alliance))) +
+  stat_pointinterval() +
   labs(x = "Predicted Support for Force",
        y = "",
-       fill = "Alliance")
+       color = "Alliance")
 
 # slopes- create groups
 slopes.treat.het <- slopes(model = tw.treat.het,
                            variables = "alliance",
-                           newdata = datagrid(model = tw.treat.het,
-                                      het.group = unique(tw.rep$het.group))
-)
+                           newdata = grid.treat.het)
 slopes.treat.het
 
+ggplot(slopes.treat.het, aes(y = estimate, x = hawk,
+                          shape = factor(male),
+                          color = factor(white))) +
+  facet_wrap(~ intl, ncol = 5) +
+  geom_pointrange(aes(ymin = conf.low, ymax = conf.high),
+                  position = position_dodge(width = 1)) +
+  scale_color_grey(name = "Race",
+                   labels = c(`0` = "Non-White",
+                              `1` = "White")) +
+  scale_shape_discrete(name = "Gender",
+                       labels = c(`0` = "Female/Other",
+                                  `1` = "Male")) +
+  labs(title = "Treatment Heterogeneity",
+       subtitle = "Internationalism, Hawkishness, Race and Gender",
+       x = "Hawkishness", 
+       y = "Marginal effect of Alliance")
+ggsave("figures/tw-treat-het.png", height = 6, width = 8)
+
+# alternative presenation
 treat.het.all <- posterior_draws(slopes.treat.het)
 
-ggplot(treat.het.all, aes(x = draw, y = het.group)) +
-  stat_halfeye() +
+ggplot(treat.het.all, aes(x = draw, y = het.group,
+                          color = factor(male),
+                          shape = factor(white))) +
+  facet_grid(hawk ~ intl, scales = "free_y") +
+  stat_pointinterval() +
   labs(x = "Marginal effect of Alliance", y = "")
+
